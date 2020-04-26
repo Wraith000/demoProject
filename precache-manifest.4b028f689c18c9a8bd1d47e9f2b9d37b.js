54632a655589749af87d8febb5afd817a53d0602
Wraith000 <Hassam.styleshop@gmail.com>
2020-04-26 23:52:29 +0500

    Updates

---
+++ precache-manifest.4b028f689c18c9a8bd1d47e9f2b9d37b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dcb4f9de137b7af83b3191fcfc9135",
+    "url": "/demoproject/index.html"
+  },
+  {
+    "revision": "04ba7a4d2e528d51e59e",
+    "url": "/demoproject/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3f0c260fd51efa64782",
+    "url": "/demoproject/static/js/2.454af0de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoproject/static/js/2.454af0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ba7a4d2e528d51e59e",
+    "url": "/demoproject/static/js/main.7193f978.chunk.js"
+  },
+  {
+    "revision": "396958f08cf25365a6bc",
+    "url": "/demoproject/static/js/runtime-main.159bc5df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
